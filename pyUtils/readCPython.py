dd1caf851f6f36116192586c6b6162297e4b10e8
Thomas Boudreaux <thomas@boudreauxmail.com>
2020-01-22 14:02:19 +0000

    Added Documentation

--- pyUtils/readCPython.py
+++ pyUtils/readCPython.py
@@ -1,7 +1,13 @@
 import numpy as np
 def load_C_output(filename):
+    # Open the file as a byte string
     with open(filename, 'rb') as f:
         contents = f.read()
+    # Read the bytes as a c type double
     state = np.frombuffer(contents, dtype=np.float64)
+
+    # Reshape to the known shape of the data (a 2D 3xn list)
+    # Using totalsize = 3*n, we know total size so we can
+    # find n
     state = state.reshape(3, int(state.shape[0]/3))
     return state
