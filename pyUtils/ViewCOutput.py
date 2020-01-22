4c44dd01f7abc4771b194ededd140578484639c9
Thomas Boudreaux <thomas@boudreauxmail.com>
2020-01-22 13:57:31 +0000

    Added Documentation

--- pyUtils/ViewCOutput.py
+++ pyUtils/ViewCOutput.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 from readCPython import load_C_output
 from getXi1 import find_root
 
@@ -12,7 +8,15 @@ import argparse
 
 import matplotlib as mpl
 
+
 def set_style(usetex=False):
+    """ set a proffesional looking matplotlib style
+
+    Keyword Arguments:
+        usetex -- use the LaTeX Rendering engine, requires
+                  LaTeX to be in the PATH
+
+    """
     plt.rc('text', usetex=usetex)
     plt.rc('font', family='Serif')
 
@@ -40,6 +44,15 @@ def set_style(usetex=False):
 
 
 def extract_theta_dot_xi(dataFiles):
+    """ return the polytroic index assosiated solution for every given binary file path
+
+    Positional Arguments:
+        dataFiles -- iterator of path to binary data files produced from c executable itegrate
+
+    Returns -> (N, STATE):
+        N -- List of poytroic indicies
+        STATE -- List of data from binary file paths (parallel to N)
+    """
     STATE = list()
     N = list()
     for dataFile in dataFiles:
@@ -49,6 +62,7 @@ def extract_theta_dot_xi(dataFiles):
 
 
 if __name__ == '__main__':
+    # Argument Parser 
     parser = argparse.ArgumentParser(description='Plot Data')
     parser.add_argument('files', metavar='<path/to/data/files>', type=str, nargs='+', help="Files to plot")
     parser.add_argument('-o', '--output', type=str, default="Figures/ThetaXi.pdf", metavar='<path/to/output/file>', help='output location')
@@ -56,21 +70,28 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--tex', action='store_true', help='Use the tex rendering engine when plotting')
 
     args = parser.parse_args()
+
+    # Filter Given paths to only select .binary files
     dataFiles = filter(lambda x: '.binary' in x, args.files)
     N, states = extract_theta_dot_xi(dataFiles)
 
+    # Set the style to include minor ticks, larger labels, and ticks on all sides
     set_style(usetex=args.tex)
 
+
+    # Plot all Given Solutions
     fig, ax = plt.subplots(1, 1, figsize=(10, 7))
     for n, state in zip(N, states):
         ax.plot(state[0], state[1], label=r"$\theta_{{n={}}}$".format(n))
         ax.plot(state[0], state[2], linestyle='--', label=r"$\left(\frac{{d\theta}}{{d\xi}}\right)_{{n={}}}$".format(n))
-    if args.root:
-        xi1Approx, theta1Approx = find_root(state[0], state[1])
-        ax.axvline(x=xi1Approx, linestyle=':', color='black', alpha=0.5)
-        ax.axhline(y=0, linestyle=':', color='black', alpha=0.5)
+        # If requested show where each solution has its root
+        if args.root:
+            xi1Approx, theta1Approx = find_root(state[0], state[1])
+            ax.axvline(x=xi1Approx, linestyle=':', color='black', alpha=0.5)
+            ax.axhline(y=0, linestyle=':', color='black', alpha=0.5)
     ax.set_xlabel(r'$\xi$', fontsize=17)
     ax.set_ylabel(r'$\theta$, $\frac{d\theta}{d\xi}$', fontsize=17)
     plt.legend()
 
+    # Save File to requested Location
     plt.savefig(args.output, bbox_inches='tight')
